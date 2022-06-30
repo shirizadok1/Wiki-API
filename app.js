389344943e7803fb583e8f1b7b73fd72cb18f42a
shirizadok1 <85099151+shirizadok1@users.noreply.github.com>
2022-06-30 16:35:26 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,121 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+
+const articleSchema = {
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+// a single route that targets all the methods for this route instead of repeating app.post("/articles") and app.get(""/articles") etc.
+//////////////////////////////// Requests Targeting All Articles/////////////////////////////
+app.route("/articles")
+
+.get(function(req, res){
+  Article.find({}, function(err, foundArticles){
+    if(!err){
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.post(function(req, res){
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err){
+    if(!err){
+      res.send("Successfuly added a new article");
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.delete(function(req, res){
+  Article.deleteMany({}, function(err){
+    if(!err){
+      res.send("Successfuly deleted all the articles.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+//////////////////////////////// Requests Targeting A specific Article/////////////////////////////
+app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if(foundArticle){
+      res.send(foundArticle);
+    } else {
+      res.send("No articles matching that title was found.");
+    }
+  });
+})
+
+.put(function(req, res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err){
+      if(!err){
+        res.send("Successfuly updated the selected article.");
+      }
+    }
+  );
+})
+
+.patch(function(req, res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body}, //This row will update only the fields in the object(title,content) that has a new vaule
+    function(err){
+      if(!err){
+        res.send("Successfuly updated article.")
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+
+.delete(function(req, res){
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+    if(!err){
+      res.send("Successfuly deleted the corresponding article.");
+    } else {
+      res.send(err);
+    }
+  }
+  );
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
